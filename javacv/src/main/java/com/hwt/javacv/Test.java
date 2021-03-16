8f1b69676ddfcf031e2d4a81de61821691fa7517
piupiuhwt <1078407859@qq.com>
2021-03-16 18:12:57 +0800

    1. 添加立体块

--- javacv/src/main/java/com/hwt/javacv/Test.java
+++ javacv/src/main/java/com/hwt/javacv/Test.java
@@ -1,6 +1,5 @@
 package com.hwt.javacv;
 
-import org.bytedeco.javacv.FFmpegFrameFilter;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FrameGrabber;
 
@@ -9,9 +8,14 @@ import java.io.File;
 public class Test {
     public static final String filePath = "D:\\workSpace\\tool\\高级Redis应用进阶课 一站式Redis解决方案";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FrameGrabber.Exception {
         File file = new File(filePath);
         FFmpegFrameGrabber ffmpegFrameGrabber = new FFmpegFrameGrabber(file);
+        ffmpegFrameGrabber.start();
         int lengthInFrames = ffmpegFrameGrabber.getLengthInFrames();
+        int i = 0;
+        while (i < lengthInFrames){
+            i++;
+        }
     }
 }
